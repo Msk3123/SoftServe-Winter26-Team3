335821dc2d8a184dfaca781ea40ed58168322e88
Ivan Hudko <giv.10072007@gmail.com>
2026-01-18 20:39:42 +0200

    Create MovieController with GetAll and Create methods. add AppDbContext to Program.cs, allow react in cors

---
+++ Cinema.Server/Cinema.Application/DTOs/MovieDto.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinema.Application.DTOs
+{
+    // GET запити
+    public record MovieDto(
+        int Id, 
+        string Title,
+        int Duration,
+        decimal Rating,
+        string PosterUrl,
+        string TrailerUrl,
+        string Language,
+        string Description,
+        DateOnly ReleaseDate,
+        List<GenreDto> Genres,
+        List<ActorDto> Actors
+    );
+
+    // POST запити
+    public record CreateMovieDto(
+        [Required(ErrorMessage = "Title is required")]
+        string Title,
+        
+        [Range(1, int.MaxValue, ErrorMessage = "Duration must be positive")]
+        int Duration,
+
+        string Description,
+
+        [Url(ErrorMessage = "Invalid Poster URL format")]
+        string PosterUrl,
+
+        [Url(ErrorMessage = "Invalid Trailer URL format")]
+        string TrailerUrl,
+
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "Language code must be exactly 2 characters(en,fr,ua etc.)")]
+        string Language,
+
+        DateOnly ReleaseDate,
+        DateOnly StartDate,
+        DateOnly EndDate,
+
+        List<int> GenreIds,
+        List<int> ActorIds
+    );
+}
