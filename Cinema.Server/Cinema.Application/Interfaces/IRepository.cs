5aa4dcf25fb222bcaa81529b5af36091debf562a
Chicago <92821486+illiadavydiuk@users.noreply.github.com>
2026-01-18 22:08:36 +0200

    add new folders + add classic repository

---
+++ Cinema.Server/Cinema.Application/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinema.Application.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        void Save();
+    }
+}
