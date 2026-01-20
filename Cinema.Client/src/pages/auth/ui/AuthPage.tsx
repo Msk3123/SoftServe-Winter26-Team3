259a4ac4151e1bfc1bb2b782c9f97befc46a0e6e
Paliy <makspalij1@gmail.com>
2026-01-20 16:44:52 +0200

    enhance auth page: add form structure and styles

--- Cinema.Client/src/pages/auth/ui/AuthPage.tsx
+++ Cinema.Client/src/pages/auth/ui/AuthPage.tsx
@@ -5,11 +5,22 @@ const AuthPage = () => {
         <div className="auth-wrapper"> {/* Загальний контейнер */}
 
             <div className="auth-banner"> {/* Контейнер 1 (лівий) */}
-                <p>Привіт</p>
+                <h1>Привіт </h1>
             </div>
 
             <div className="auth-form-container"> {/* Контейнер 2 (правий) */}
-               
+                <div className="auth-form">
+               <h2>Create account</h2>
+                <form>
+                    <p>Email</p>
+                    <input type="text" placeholder="Email" />
+                    <p>Password</p>
+                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Password" />
+                    <button type="submit">Sign up</button>
+                    <p>Already have an account? <a href="#">Sign in</a></p>
+                    </form>
+                </div>
             </div>
 
         </div>
