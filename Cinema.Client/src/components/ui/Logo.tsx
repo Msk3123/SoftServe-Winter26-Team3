a38a090a1a619c1c447968f5e336eb0d4a3871db
Ivan Hudko <giv.10072007@gmail.com>
2026-01-21 16:06:01 +0200

    add Logo component

--- Cinema.Client/src/components/ui/Logo.tsx
+++ Cinema.Client/src/components/ui/Logo.tsx
@@ -1,6 +1,21 @@
-const Logo: React.FC = ()=>{
+import logoUrl from '../../assets/logo.svg';
+
+interface Props{
+    size?: "extraSmall"|"small"|"regular"|"large";
+}
+
+    const dimensions = {
+        extraSmall: { width: 40, height: 15 },
+        small: { width: 60, height: 22 },
+        regular: { width: 130, height: 49 },
+        large:{ width: 195, height: 74 },
+        extralarge:{ width: 260, height: 98 }
+    };
+
+const Logo: React.FC<Props> = ({size="regular"})=>{
+    const {width,height} = dimensions[size]
     return(
-        <span>Team<span style={{color:"var(--color-primary)"}}>3</span></span>
+        <img src={logoUrl} alt="cinema logo" width={width} height={height} />
     )
 }
 
