aa75f4c755a17dbeba04dc7b6c676b07728f4673
Chicago <92821486+illiadavydiuk@users.noreply.github.com>
2026-01-22 22:58:40 +0200

    add role controller and all functions

---
+++ Cinema.Server/Cinema.Application/DTOs/RoleDtos/RoleCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinema.Application.DTOs.RoleDtos
+{
+    public record RoleCreateDto
+    {
+        public string RoleName { get; set; }
+    }
+}
---
+++ Cinema.Server/Cinema.Application/Interfaces/IRoleRepository.cs
@@ -0,0 +1,11 @@
+﻿using Cinema.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinema.Application.Interfaces
+{
+    public interface IRoleRepository : IRepository<Role>
+    {
+    }
+}
