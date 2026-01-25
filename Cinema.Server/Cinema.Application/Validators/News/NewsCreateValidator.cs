b6b89fc361ba4ae3bd1c7ba09f7325efa5421a68
Chicago <92821486+illiadavydiuk@users.noreply.github.com>
2026-01-25 21:25:05 +0200

    add news create and patch validators

---
+++ Cinema.Server/Cinema.Application/Validators/News/NewsCreateValidator.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using Cinema.Application.DTOs.NewsDtos;
+using Cinema.Application.Common.Extensions;
+
+namespace Cinema.Application.Validators.News
+{
+    public class NewsCreateValidator : AbstractValidator<NewsCreateDto>
+    {
+        public NewsCreateValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Заголовок новини обов'язковий.")
+                .MaximumLength(200).WithMessage("Заголовок не може перевищувати 200 символів.");
+
+            RuleFor(x => x.NewsContent)
+                .NotEmpty().WithMessage("Контент новини не може бути порожнім.");
+
+            RuleFor(x => x.ImageUrl)
+                .NotEmpty().WithMessage("Посилання на зображення обов'язкове.")
+                .IsValidUrl().WithMessage("Некоректний формат URL для зображення.");
+
+            RuleFor(x => x.PublishedDate)
+                .NotEmpty().WithMessage("Дата публікації обов'язкова.")
+                .Must(date => date.Date >= DateTime.Today)
+                .WithMessage("Дата публікації не може бути в минулому.");
+
+            RuleFor(x => x.TagId)
+                .GreaterThan(0).WithMessage("TagId має бути більшим за 0.");
+
+            RuleFor(x => x.MovieId)
+                .GreaterThan(0).WithMessage("MovieId має бути більшим за 0.")
+                .When(x => x.MovieId.HasValue);
+
+            RuleFor(x => x.ActorId)
+                .GreaterThan(0).WithMessage("ActorId має бути більшим за 0.")
+                .When(x => x.ActorId.HasValue);
+        }
+    }
+}
---
+++ Cinema.Server/Cinema.Application/Validators/News/NewsPatchValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using Cinema.Application.DTOs.NewsDtos;
+using Cinema.Application.Common.Extensions;
+
+namespace Cinema.Application.Validators.News
+{
+    public class NewsPatchValidator : AbstractValidator<NewsPatchDto>
+    {
+        public NewsPatchValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Заголовок не може бути порожнім.")
+                .MaximumLength(200).WithMessage("Заголовок не може перевищувати 200 символів.")
+                .When(x => x.Title != null);
+
+            RuleFor(x => x.ImageUrl)
+                .IsValidUrl().WithMessage("Некоректний формат URL.")
+                .When(x => x.ImageUrl != null);
+
+            RuleFor(x => x.TagId)
+                .GreaterThan(0).WithMessage("TagId має бути більшим за 0.")
+                .When(x => x.TagId.HasValue);
+
+            RuleFor(x => x.MovieId)
+                .GreaterThan(0).WithMessage("MovieId має бути більшим за 0.")
+                .When(x => x.MovieId.HasValue);
+
+            RuleFor(x => x.ActorId)
+                .GreaterThan(0).WithMessage("ActorId має бути більшим за 0.")
+                .When(x => x.ActorId.HasValue);
+        }
+    }
+}
