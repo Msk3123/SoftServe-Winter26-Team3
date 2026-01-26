9f9142124854374df54b74c624a7be193e3b6945
Ivan Hudko <giv.10072007@gmail.com>
2026-01-26 16:28:37 +0200

    add toast for deleting rows

---
+++ Cinema.Client/src/components/ConfirmDelete/confirmDelete.tsx
@@ -0,0 +1,37 @@
+import toast from 'react-hot-toast';
+import styles from "./ConfirmDelete.module.css"
+import Button from '../Button/Button';
+const confirmDelete = (message="Are you sure you want to delete that") => {
+  return new Promise((resolve) => {
+  toast((t) => (
+    <span>
+      {message}
+      <div className={styles.toastContainer}>
+        <Button
+        bgColor='var(--color-danger)'
+          action={async () => {
+            toast.dismiss(t.id);
+            resolve(true);
+          }}
+        >
+          Delete
+        </Button>
+        <Button
+        bgColor='var(--button-cancel)'
+          action={() =>{
+            toast.dismiss(t.id)
+            resolve(false);
+          }}
+        >
+          Cancel
+        </Button>
+      </div>
+    </span>
+  ), {
+    duration: 6000,
+    position: 'top-center',
+  });
+});
+};
+
+export default confirmDelete;
