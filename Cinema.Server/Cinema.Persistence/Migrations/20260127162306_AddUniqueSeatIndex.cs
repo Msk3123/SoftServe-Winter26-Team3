1a96c39d3e9d1fe121870075c11505c3f8d27f0d
Walpys <artemkrat073@gmail.com>
2026-01-27 20:05:21 +0200

    added seat controller and necessary parts, updated ExeptionMiddleware, added AsyncValidationFilter, QueryParametersValidator, database migration

---
+++ Cinema.Server/Cinema.Persistence/Migrations/20260127162306_AddUniqueSeatIndex.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cinema.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueSeatIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Seats_HallId",
+                table: "Seats");
+
+            migrationBuilder.RenameColumn(
+                name: "OrderIid",
+                table: "Orders",
+                newName: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seat_HallId_SeatNo_Unique",
+                table: "Seats",
+                columns: new[] { "HallId", "SeatNo" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Seat_HallId_SeatNo_Unique",
+                table: "Seats");
+
+            migrationBuilder.RenameColumn(
+                name: "OrderId",
+                table: "Orders",
+                newName: "OrderIid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seats_HallId",
+                table: "Seats",
+                column: "HallId");
+        }
+    }
+}
