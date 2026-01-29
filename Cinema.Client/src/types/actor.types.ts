4d5de4f66d99d904f8cb4fda4adbe8376e9c79db
Ivan Hudko <giv.10072007@gmail.com>
2026-01-29 18:14:40 +0200

    add actors to admin panel

--- Cinema.Client/src/types/actor.types.ts
+++ Cinema.Client/src/types/actor.types.ts
@@ -5,12 +5,16 @@ export interface ActorShort{
     photoUrl: string,
 }
 
-export interface Actor{
-    id: number|string,
-    firstName: string,
-    lastName: string,
-    photoUrl: string,
-    biography: string,
-    birthday: string,
-    movies:string[],
+export interface ActorCreate {
+    firstName: string;
+    lastName: string;
+    biography: string;
+    birthday: string;
+    photoUrl: string;
+}
+
+
+export interface Actor extends ActorCreate{
+    id: number|string;
+    movies:string[];
 }
