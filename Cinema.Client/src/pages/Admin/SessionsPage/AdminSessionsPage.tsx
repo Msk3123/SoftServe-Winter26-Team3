cce28dcd586533d5b31629aa78faef36bb204676
Ivan Hudko <giv.10072007@gmail.com>
2026-01-29 18:20:51 +0200

    change poster col position , make logo navlink to home

--- Cinema.Client/src/pages/Admin/SessionsPage/AdminSessionsPage.tsx
+++ Cinema.Client/src/pages/Admin/SessionsPage/AdminSessionsPage.tsx
@@ -10,13 +10,13 @@ const AdminMoviesPage : React.FC = ()=>{
     
     const columns:ColumnDef<SessionShort>[] = [
     { key: "id", title: "№" },
-    { key: "movieTitle", title: "Movie" },
-    { key: "sessionDate", title: "Date" , render:(item)=>dateToString(new Date(item.sessionDate))},
-    { key: "sessionTime", title: "Time", render:(item)=> item.sessionTime.slice(0,5)},
-    { key: "hallName", title: "Hall" },
     { key: "posterUrl", title: "Poster",render:(item)=>{
         return <img src={item.posterUrl} alt={`${item.movieTitle} poster`} className={styles.imageCell}/>
     } },
+    { key: "movieTitle", title: "Movie" },
+    { key: "sessionDate", title: "Date" , render:(item)=>dateToString(new Date(item.sessionDate))},
+    { key: "sessionTime", title: "Time", render:(item)=> item.sessionTime.slice(0,5)},
+    { key: "hallName", title: "Hall" }
 ];
     return( <>
                 <AdminTablePage columns={columns} queryFn={getAllSessions} deleteFn={deleteSession}/>
