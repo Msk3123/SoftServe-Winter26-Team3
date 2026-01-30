28735c6ec9661a04e4c5679c098d9403cd4353d1
Vlad <kononvlad007@gmail.com>
2026-01-30 16:18:52 +0200

    feat: add ticketType controller, with CRUD functionality and validation

---
+++ Cinema.Server/Cinema.API/Controllers/TicketTypeController.cs
@@ -0,0 +1,73 @@
+using AutoMapper;
+using Cinema.Application.DTOs.TicketTypeDtos;
+using Cinema.Application.Interfaces;
+using Cinema.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Cinema.Application.Common.Models;
+
+
+namespace Cinema.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TicketTypesController : ApiBaseController
+    {
+        private readonly ITicketTypeRepository _ticketTypeRepository;
+
+        public TicketTypesController(ITicketTypeRepository repository, IMapper mapper)
+             : base(mapper)
+        {
+            _ticketTypeRepository = repository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] QueryParameters queryParameters)
+        {
+            var results = await _ticketTypeRepository.GetAllPagedAsync(queryParameters);
+            return OkPaged<TicketType, TicketTypeDto>(results, queryParameters);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TicketTypeDto>> GetById(int id)
+        {
+            var ticketType = await _ticketTypeRepository.GetByIdAsync(id);
+            if (ticketType == null) return NotFound();
+
+            return Ok(_mapper.Map<TicketTypeDto>(ticketType));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TicketTypeDto>> Create(TicketTypeCreateDto dto)
+        {
+            var ticketType = _mapper.Map<TicketType>(dto);
+            await _ticketTypeRepository.AddAsync(ticketType);
+            await _ticketTypeRepository.SaveAsync();
+            var result = _mapper.Map<TicketTypeDto>(ticketType);
+            return CreatedAtAction(nameof(GetById), new { id = ticketType.TicketTypeId }, result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, TicketTypeCreateDto dto)
+        {
+            var ticketType = await _ticketTypeRepository.GetByIdAsync(id);
+            if (ticketType == null) return NotFound();
+
+            _mapper.Map(dto, ticketType);
+
+            await _ticketTypeRepository.SaveAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var ticketType = await _ticketTypeRepository.GetByIdAsync(id);
+            if (ticketType == null) return NotFound();
+            await _ticketTypeRepository.DeleteAsync(id);
+            await _ticketTypeRepository.SaveAsync();
+
+            return NoContent();
+        }
+    }
+}
