d5383baf1f293cb6f2202646586e7940b36b9009
Chicago <92821486+illiadavydiuk@users.noreply.github.com>
2026-01-31 22:52:35 +0200

    refactor patch

--- Cinema.Server/Cinema.Application/Mappings/TicketTypeMapper.cs
+++ Cinema.Server/Cinema.Application/Mappings/TicketTypeMapper.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using Cinema.Application.DTOs.HallDtos;
 using Cinema.Application.DTOs.TicketTypeDtos;
 using Cinema.Domain.Entities;
 
@@ -8,13 +9,16 @@ namespace Cinema.Application.Mappings
     {
         public TicketTypeMapper()
         {
-
             CreateMap<TicketType, TicketTypeDto>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.TicketTypeId));
             CreateMap<TicketTypeCreateDto, TicketType>();
 
             CreateMap<TicketTypePatchDto, TicketType>()
-                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+                 .ForAllMembers(opts => opts.Condition((src, dest, srcMember) =>
+                 srcMember != null &&
+                 !(srcMember is int i && i == 0) &&
+                 !(srcMember is decimal d && d == 0)));
+
         }
     }
 }
