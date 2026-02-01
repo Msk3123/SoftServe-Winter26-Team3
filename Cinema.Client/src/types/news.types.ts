5138784d46c224ffec9c68834291ed40151af4f3
Ivan Hudko <giv.10072007@gmail.com>
2026-02-01 16:11:10 +0200

    create newsApi methods

--- Cinema.Client/src/types/news.types.ts
+++ Cinema.Client/src/types/news.types.ts
@@ -5,3 +5,24 @@ export interface NewsShort {
   imageUrl: string;
   publishedDate: string;
 }
+export interface NewsCreate{
+  title: string;
+  newsContent: string;
+  imageUrl: string;
+  publishedDate: string;
+  tagId: number;
+  isActive: boolean;
+  movieId?: number;
+  actorId?: number;
+}
+export interface News {
+  id: number;
+  title: string;
+  newsContent: string;
+  imageUrl: string;
+  publishedDate: string;
+  isActive: boolean;
+  tagName: string;
+  movieTitle: string | null;
+  actorFullName: string | null;
+}
