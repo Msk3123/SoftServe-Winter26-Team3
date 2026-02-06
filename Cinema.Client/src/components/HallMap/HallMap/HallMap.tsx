6906880a061cecae0864f7cf4e3c848ad554b51c
Ivan Hudko <giv.10072007@gmail.com>
2026-02-06 12:49:22 +0200

    fix styles problems

--- Cinema.Client/src/components/HallMap/HallMap/HallMap.tsx
+++ Cinema.Client/src/components/HallMap/HallMap/HallMap.tsx
@@ -14,6 +14,7 @@ const HallMap = <T extends { id: number | string }>({
     onMouseUp
 }: HallMapProps<T>)=>{
     return (
+    <div className={styles.wrapper}>
         <div
             className={styles.container}
             onMouseDown={onMouseDown}
@@ -22,16 +23,21 @@ const HallMap = <T extends { id: number | string }>({
         >
             {/* cols numbers */}
             <div className={styles.rowDiff}>
-                <div className={styles.rowNumber}></div>
+                <div className={`${styles.cell} ${styles.rowNumber}`}></div>
                 
-                {Array.from({length:seats[0].length}).map((_,i)=><div className={styles.colNumber}  key={`col-humber-1-${i}`}>{ i + 1}</div>)}
-                
-                <div className={styles.rowNumber}></div>
+                    {Array.from({length:seats[0].length}).map((_,i)=>(
+                        <div
+                            className={`${styles.cell} ${styles.colNumber}`}
+                            key={`col-humber-1-${i}`}>
+                                { i + 1}
+                        </div>
+                        ))}
+                <div className={`${styles.cell} ${styles.rowNumber}`}></div>
             </div>
 
             {seats.map((row,i)=>(
                 <div key={`row ${i}`} className={styles.row}>
-                    <div className={styles.rowNumber} key={`row-humber-${i}`}>{ i + 1}</div>
+                    <div className={`${styles.cell} ${styles.rowNumber}`}  key={`row-humber-${i}`}>{ i + 1}</div>
                     
                     {row.map((seat)=> 
                         <Fragment key={seat.id}>
@@ -39,20 +45,25 @@ const HallMap = <T extends { id: number | string }>({
                         </Fragment>
                     )}
 
-                    <div className={styles.rowNumber} key={`row-humber-2-${i}`}>{ i + 1}</div>
+                    <div className={`${styles.cell} ${styles.rowNumber}`}  key={`row-humber-2-${i}`}>{ i + 1}</div>
                 </div>
             ))}
 
             {/* bottom cols numbers */}
             <div className={styles.rowDiff}>
-                <div className={styles.colNumber}></div>
+                <div className={`${styles.cell} ${styles.rowNumber}`}></div>
                 
-                {Array.from({length:seats[0].length}).map((_,i)=><div className={styles.colNumber} key={`col-humber-2-${i}`}>{ i+ 1}</div>)}
-                
-                <div className={styles.colNumber}></div>
+                    {Array.from({length:seats[0].length}).map((_,i)=>(
+                        <div
+                            className={`${styles.cell} ${styles.colNumber}`}
+                            key={`col-humber-2-${i}`}>
+                                { i + 1}
+                        </div>
+                        ))}
+                <div className={`${styles.cell} ${styles.rowNumber}`}></div>
             </div>
-
         </div>
+    </div>
     )
 }
 
--- Cinema.Client/src/components/HallMap/HallMapSceleton/HallMapSceleton.tsx
+++ Cinema.Client/src/components/HallMap/HallMapSceleton/HallMapSceleton.tsx
@@ -7,29 +7,53 @@ interface HallMapSceletonProps{
 }
 const HallMapSceleton = ({rows,seatsPerRow,className=""}:HallMapSceletonProps)=>{
     return (
+    <div className={styles.wrapper}>
         <div className={`${styles.container} ${className}`}>
             <div className={styles.rowDiff}>
-                <div className={styles.rowNumber}></div>
-                {Array.from({length:seatsPerRow}).map((_,i)=><div className={styles.colNumber}  key={`col-humber-1-${i}`}>{ i + 1}</div>)}
-                <div className={styles.rowNumber}></div>
+                <div className={`${styles.cell} ${styles.rowNumber}`}></div>
+                
+                    {Array.from({length:seatsPerRow}).map((_,i)=>(
+                        <div
+                            className={`${styles.cell} ${styles.colNumber}`}
+                            key={`col-humber-1-${i}`}>
+                                { i + 1}
+                        </div>
+                        ))}
+                
+                <div className={`${styles.cell} ${styles.rowNumber}`}></div>
             </div>
 
             {Array.from({length:rows}).map((_,i)=>(
                 <div key={`row ${i}`} className={styles.row}>
-                    <div className={styles.rowNumber} key={`row-humber-1-${i}`}>{ i + 1}</div>
+                    <div className={`${styles.cell} ${styles.rowNumber}`} key={`row-humber-1-${i}`}>{ i + 1}</div>
+                    
                     {Array.from({length:seatsPerRow}).map((_,j)=> (
-                        <div className={styles.seat} key={`row ${i} seat ${j}`} ></div>
+                        <div
+                            className={`${styles.cell} ${styles.seat}`} 
+                            key={`row ${i} seat ${j}`} >
+                        </div>
                     ))}
-                    <div className={styles.rowNumber} key={`row-humber-2-${i}`}>{ i + 1}</div>
+
+                    <div className={`${styles.cell} ${styles.rowNumber}`} key={`row-humber-2-${i}`}>{ i + 1}</div>
                 </div>
             ))}
             
             <div className={styles.rowDiff}>
-                <div className={styles.rowNumber}></div>
-                {Array.from({length:seatsPerRow}).map((_,i)=><div className={styles.colNumber} key={`col-humber-2-${i}`}>{ i + 1}</div>)}
-                <div className={styles.rowNumber}></div>
+                <div className={`${styles.cell} ${styles.rowNumber}`}></div>
+                    
+                {Array.from({length:seatsPerRow}).map((_,i)=>(
+                    <div
+                        className={`${styles.cell} ${styles.colNumber}`} 
+                        key={`col-humber-2-${i}`}
+                    >
+                        { i + 1}
+
+                    </div>))}
+                
+                <div className={`${styles.cell} ${styles.rowNumber}`}></div>
             </div>
         </div>
+    </div>
     )
 }
 
