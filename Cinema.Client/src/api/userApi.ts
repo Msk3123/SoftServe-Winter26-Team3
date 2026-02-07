85aacc29f9fb9f2c22f71ebfeede5615a5f68f78
Ivan Hudko <giv.10072007@gmail.com>
2026-02-07 16:22:48 +0200

    define types and CRUD methods for user

--- Cinema.Client/src/api/userApi.ts
+++ Cinema.Client/src/api/userApi.ts
@@ -1,7 +1,23 @@
-import type { FetchOneFunction } from "../types/api.types";
-import type { UserDetails } from "../types/user.types";
-import { getItem } from "./api";
+import type { DeleteFunction, FetchOneFunction, FetchPaginatedListFunction, PatchFunction, PostFunction, PutFunction } from "../types/api.types";
+import type { User, UserCreate } from "../types/user.types";
+import { defaultParams, deleteItem, getItem, getPaginatedData, patchItem, postItem, putItem } from "./api";
 
-export const getUser: FetchOneFunction<UserDetails> = async (id) => {
+export const getAllUsers :FetchPaginatedListFunction<User> = async (params=defaultParams)=>{
+    return await getPaginatedData("users",params)
+}
+export const getUser: FetchOneFunction<User> = async (id) => {
     return await getItem("users", id);
+};
+
+export const postUser: PostFunction<UserCreate,User> = async (data)=>{
+    return await postItem("users",data);
+}
+export const putUser:PutFunction<UserCreate> = async(id,data)=>{
+    return await putItem("users",id,data);
+}
+export const patchUser:PatchFunction<UserCreate> = async(id,data)=>{
+    return await patchItem("users",id,data);
+}
+export const deleteUser:DeleteFunction = async (id)=>{
+    return deleteItem("users",id);
 };
